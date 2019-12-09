92fd1463a998cba3f08525b6c633025272b93684
陳涵宇 <rollr76518@gmail.com>
2019-12-09 23:42:58 +0800

    Finish API Search user response

--- GithubSearchAPIPracticeTests/GithubSearchAPIPracticeTests.swift
+++ GithubSearchAPIPracticeTests/GithubSearchAPIPracticeTests.swift
@@ -35,4 +35,37 @@ class GithubSearchAPIPracticeTests: XCTestCase {
 		}
 	}
 
+	func testParseUsersResponse() {
+		expectation(description: "Testing parse users response", timeout: 15.0) { (done) in
+			guard let filePath = Bundle(for: Swift.type(of: self)).path(forResource: "api_search_users", ofType: "json"),
+				let data = try? Data(contentsOf: URL(fileURLWithPath: filePath)) else {
+					print("file is not exist or something wrong.")
+					return
+			}
+			
+			let decoder = JSONDecoder()
+			decoder.keyDecodingStrategy = .convertFromSnakeCase
+
+			do {
+				let response = try decoder.decode(APIResponseObject<User>.self, from: data)
+				XCTAssertNotNil(response)
+				
+				XCTAssertEqual(response.totalCount, 40)
+				XCTAssertEqual(response.incompleteResults, false)
+				
+				guard let first = response.items.first else {
+					XCTFail("Response items should have a item at least.")
+					done()
+					return
+				}
+				
+				XCTAssertEqual(first.avatarUrl.absoluteString, "https://avatars0.githubusercontent.com/u/34742481?v=4")
+				XCTAssertEqual(first.login, "Rollr-io")
+			} catch {
+				XCTFail(error.localizedDescription)
+			}
+			
+			done()
+		}
+	}
 }
